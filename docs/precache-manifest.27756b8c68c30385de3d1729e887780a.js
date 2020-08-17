adbf03ad9ee9c555f93b2449642d9cfd8b5fae73
Nicolas Ponce <nponcech@gmail.com>
2020-08-17 18:17:26 -0400

    docs agregado

---
+++ docs/precache-manifest.27756b8c68c30385de3d1729e887780a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587d423813202aa0b95d00b546900858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fa318c64ad10502973a",
+    "url": "/static/css/main.242b45fc.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa318c64ad10502973a",
+    "url": "/static/js/main.787ebd21.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
